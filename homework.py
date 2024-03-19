d067ae1e27891ee54bc8cb81e68d8dcb43970ad3
adonaione <adonairomero@icloud.com>
2024-03-19 15:56:14 -0600

    almost done, getting bug on line 72, cart.add(food1)

--- homework.py
+++ homework.py
@@ -1,14 +1,14 @@
 # Exercise 1 - Write a Python class for an Animal that has a name and energy attributes. The animal class should also have methods for eat, sleep, and play that will take in an integer and increase/decrease the energy of the animal with a formatted print statement
 
-
-class Animals():
+# Example 1
+#create animal class
+class Animal():
     def __init__(self, name, energy=100):
         self.name = name
         self.energy = energy
     def eat(self, food):
         self.energy = self.energy + (food * 10)
-        # self.energy += food
-        print(f'{self.name} has ate {food} blocks of food. His energy is now {self.energy}')
+        print(f'{self.name} has ate {food} cans of food. His energy is now {self.energy}')
     def sleep(self, sleep):
         self.energy = self.energy + sleep
         print(f"{self.name} has slept for {sleep} minutes. His energy is now {self.energy}")
@@ -22,7 +22,7 @@ dog = Animal('Buddy', 10)
 puppy = Animal('Lotus')
 dog.play(5)  # -> "Buddy is playing for 5 minutes. His energy is now 5"
 dog.sleep(10) # -> "Buddy is sleeping for 10 minutes. His energy is now 15"
-puppy.play(30)
+puppy.play(60)
 puppy.eat(2)
 puppy.sleep(30)
 
@@ -31,41 +31,43 @@ puppy.sleep(30)
 # Exercise 2 - Turn the shopping cart program into an object-oriented program
 
 # Create a class called cart that retains items and has methods to add, remove, and show
-
+#create class for cart
 class Cart:
+    #def initalize cart with list to retain items 
+    def __init__(self):
+        # create empty list
+        self.shopping_list = []
     
+    #create method for adding
+    def add(self, item):
+        #add item to list
+        lst = self.shopping_list.append(item)
+        return lst
     
-    def __init__(self, item, price):
-        self.item = item
-        self.price = price
-        self.shopping_cart = []
-
-    #Create a function for adding items, removing items, and showing the cart
-    def add(self, food):
-        food = input("What would you like to add to your cart today?")
-        self.shopping_cart.append(food)
-        print(f'{food.name.title()} has been added to your cart.')
-
-
-    def remove(self, food):
-        self.shopping_cart.remove(food)
-        print(f'{food.name} has been removed from your cart.')
-
+    # create method for removing
+    def remove(self, item):
+        # remove item from list
+        lst = self.shopping_list.remove(item)
+        return lst
+    
+    #create method for showing items in cart
     def show(self):
-        print(f'Your Shopping Cart: \n{self.shopping_cart}')
+        # print list
+        print(self.shopping_list)
+        
+
+class food:
+    def __init__(self, item, quantity):
+        self.item = item 
+        self.quantity = quantity    
 
 
-class dfood:
-    def __init__(self, name, quantity):
-        self.name = name
-        self.quantity = quantity
 
+food1 = food("Apples", 8)
+food2 = food("Oranges", 3)
+food3 = food("Coconuts", 3)
+food4 = food("Papaya", 4)
 
-
-food1 = dfood("Apples", 8)
-food2 = dfood("Oranges", 3)
-food3 = dfood("Coconuts", 3)
-food4 = dfood("Papaya", 4)
 
 Cart.add(food1)
 Cart.show()
